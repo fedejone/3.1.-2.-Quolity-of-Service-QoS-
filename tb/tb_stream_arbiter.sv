03e4d763211a4759c2e7ef597cd343394955350b
fedejone <131912870+fedejone@users.noreply.github.com>
2024-01-07 06:35:22 +0300

    Update tb_stream_arbiter.sv

--- tb/tb_stream_arbiter.sv
+++ tb/tb_stream_arbiter.sv
@@ -22,190 +22,80 @@ module tb_stream_arbiter ();
   logic                    m_valid_o;
   logic                    m_ready_i;
 
-generate
-  if (LATENCY_SLAVE == 1) begin
+  generate
+    if (LATENCY_SLAVE == 1) begin  // смена режимов слейва
 
-    stream_arbiter_latency_0 #(
-      .T_DATA_WIDTH(T_DATA_WIDTH),
-      .T_QOS__WIDTH(T_QOS__WIDTH),
-      .STREAM_COUNT(STREAM_COUNT)
-    ) uut_stream_arbiter (
-      .clk_i    (clk_i    ),
-      .rst_n    (rst_n    ),
-      .s_data_i (s_data_i ),
-      .s_qos_i  (s_qos_i  ),
-      .s_last_i (s_last_i ),
-      .s_valid_i(s_valid_i),
-      .s_ready_o(s_ready_o),
-      .m_data_o (m_data_o ),
-      .m_qos_o  (m_qos_o  ),
-      .m_id_o   (m_id_o   ),
-      .m_last_o (m_last_o ),
-      .m_valid_o(m_valid_o),
-      .m_ready_i(m_ready_i)
-    );
+      stream_arbiter_latency_0 #(
+        .T_DATA_WIDTH(T_DATA_WIDTH),
+        .T_QOS__WIDTH(T_QOS__WIDTH),
+        .STREAM_COUNT(STREAM_COUNT)
+      ) uut_stream_arbiter (
+        .clk_i    (clk_i    ),
+        .rst_n    (rst_n    ),
+        .s_data_i (s_data_i ),
+        .s_qos_i  (s_qos_i  ),
+        .s_last_i (s_last_i ),
+        .s_valid_i(s_valid_i),
+        .s_ready_o(s_ready_o),
+        .m_data_o (m_data_o ),
+        .m_qos_o  (m_qos_o  ),
+        .m_id_o   (m_id_o   ),
+        .m_last_o (m_last_o ),
+        .m_valid_o(m_valid_o),
+        .m_ready_i(m_ready_i)
+      );
 
-  end else begin
-    stream_arbiter_latency_1 #(
-      .T_DATA_WIDTH(T_DATA_WIDTH),
-      .T_QOS__WIDTH(T_QOS__WIDTH),
-      .STREAM_COUNT(STREAM_COUNT)
-    ) uut_stream_arbiter (
-      .clk_i    (clk_i    ),
-      .rst_n    (rst_n    ),
-      .s_data_i (s_data_i ),
-      .s_qos_i  (s_qos_i  ),
-      .s_last_i (s_last_i ),
-      .s_valid_i(s_valid_i),
-      .s_ready_o(s_ready_o),
-      .m_data_o (m_data_o ),
-      .m_qos_o  (m_qos_o  ),
-      .m_id_o   (m_id_o   ),
-      .m_last_o (m_last_o ),
-      .m_valid_o(m_valid_o),
-      .m_ready_i(m_ready_i)
-    );
-  end
+    end else begin
+      stream_arbiter_latency_1 #(
+        .T_DATA_WIDTH(T_DATA_WIDTH),
+        .T_QOS__WIDTH(T_QOS__WIDTH),
+        .STREAM_COUNT(STREAM_COUNT)
+      ) uut_stream_arbiter (
+        .clk_i    (clk_i    ),
+        .rst_n    (rst_n    ),
+        .s_data_i (s_data_i ),
+        .s_qos_i  (s_qos_i  ),
+        .s_last_i (s_last_i ),
+        .s_valid_i(s_valid_i),
+        .s_ready_o(s_ready_o),
+        .m_data_o (m_data_o ),
+        .m_qos_o  (m_qos_o  ),
+        .m_id_o   (m_id_o   ),
+        .m_last_o (m_last_o ),
+        .m_valid_o(m_valid_o),
+        .m_ready_i(m_ready_i)
+      );
+    end
   endgenerate
 
-  integer       count         = 0;
-  logic         ready_delay_0    ;
-  logic         ready_delay_1    ;
-  logic   [7:0] data_1           ;
-  logic   [7:0] data_2           ;
+  logic                    ready_delay_0;
+  logic                    ready_delay_1;
+  logic [             7:0] data_1       ;
+  logic [             7:0] data_2       ;
+  logic [T_QOS__WIDTH-1:0] qos_1        ;
+  logic [T_QOS__WIDTH-1:0] qos_2        ;
 
+  typedef struct {
+    logic [T_DATA_WIDTH-1:0] sdata;
+    logic [T_QOS__WIDTH-1:0] id  ;
+    logic [T_QOS__WIDTH-1:0] qos;
+  } packet;
 
+  mailbox#(packet) in = new();
+  mailbox#(packet) out = new();
 
   task wait_clk(int i);
     repeat (i) @(posedge clk_i);
   endtask : wait_clk
-
-task test_arbiter_1(int i);
-  while (count >= 0) begin
-    if (count <= 2) begin
-      m_ready_i <= 1;
-      s_valid_i <= '0;
-      s_qos_i[0] <= 1;
-      s_qos_i[1] <= 5;
-      count++;
-      wait_clk(1);
-    end else if (count >= 2 && count <= 4 ) begin
-      count++;
-      s_valid_i[1] <= 0;
-      wait_clk(1);
-    end else if (count >= 2 && count <= 4 && count <= 10) begin
-      count++;
-      s_valid_i[1] <= 0;
-      wait_clk(1);
-    end else if ( count >=4 && count <= 10) begin
-      if (count == 5 || count == 10) begin
-        s_last_i[0] <= 1;
-        s_last_i[1] <= 1;
-        s_qos_i[0] <= $urandom_range(0,15);
-        s_qos_i[1] <= $urandom_range(0,15);
-        wait_clk(1);
-        s_last_i[0] <= 0;
-        s_last_i[1] <= 0;
-      end
-      count++;
-      s_valid_i[0] <= 0;
-      s_valid_i[1] <= 1;
-      wait_clk(1);
-    end else if ( count >= 4 && count <= 10) begin
-      if (count == 10 || count == 5) begin
-        s_last_i[1] <= 1;
-        s_qos_i[0] <= 2;
-        s_qos_i[1] <= 1;
-        count++;
-        wait_clk(1);
-        s_last_i[1] <= 0;
-      end
-      count++;
-      s_valid_i[0] <= 1;
-      s_valid_i[1] <= 1;
-      wait_clk(1);
-    end else if ( count >= 10 && count <= 15  ) begin
-      if (count == 15 ) begin
-        s_last_i[0] <= 1;
-        s_last_i[1] <= 1;
-        s_qos_i[0] <= 0;
-        s_qos_i[1] <= 0;
-        wait_clk(1);
-        s_last_i[0] <= 0;
-        s_last_i[1] <= 0;
-      end
-      count++;
-      s_valid_i[0] <= 1;
-      s_valid_i[1] <= 1;
-      wait_clk(1);
-    end else if ( count >= 15 && count <= 20 ) begin
-      if (count == 20 ) begin
-        s_last_i[0] <= 1;
-        s_qos_i[0] <= 0;
-        s_qos_i[1] <= 0;
-        wait_clk(1);
-        s_last_i[0] <= 0;
-      end
-      count++;
-      s_valid_i[0] <= 1;
-      s_valid_i[1] <= 1;
-      wait_clk(1);
-    end else if ( count >= 20 && count <= 25 ) begin
-      if (count == 25 ) begin
-        s_last_i[0] <= 1;
-        s_qos_i[0] <= 0;
-        s_qos_i[1] <= 4;
-        wait_clk(1);
-        s_last_i[0] <= 0;
-      end
-      count++;
-      s_valid_i[0] <= 1;
-      s_valid_i[1] <= 1;
-      wait_clk(1);
-    
-  end else if ( count >= 25 ) begin
-      if (count == 30 ) begin
-        s_last_i[0] <= 1;
-        s_qos_i[0] <= 0;
-        s_qos_i[1] <= 0;
-        wait_clk(1);
-        s_last_i[0] <= 0;
-      end
-      count++;
-      s_valid_i[0] <= 1;
-      s_valid_i[1] <= 1;
-      wait_clk(1);
-    end
-    if (count == 40)
-      break;
-  end
-endtask : test_arbiter_1
-
-
-    initial begin
-      forever begin
-        ready_delay_0 <= s_ready_o[0];
-        ready_delay_1 <= s_ready_o[1];
-        wait_clk(1);
-      end
-    end 
-
+ /*------------------------------------------------------------------------------
+ --  начальные значения
+ ------------------------------------------------------------------------------*/
   initial begin
     clk_i = 0;
     rst_n = 1;
     s_last_i = '0;
-    forever #2 clk_i = ~clk_i ; 
+    forever #2 clk_i = ~clk_i ;
   end
-
-  always_ff @(posedge clk_i or negedge rst_n) begin : proc_s_data_i
-     begin 
-      s_data_i[0] <= $urandom_range(0,10);
-      s_data_i[1] <= $urandom_range(0,10);
-      data_1        <= s_data_i[1];
-      data_2        <= s_data_i[0];
-    end
-  end
-
 
   initial begin
     wait_clk(1);
@@ -214,15 +104,88 @@ endtask : test_arbiter_1
     m_ready_i <= '1;
     rst_n = 1;
     wait_clk(1);
-    test_arbiter_1(1);
-    wait_clk(4);
-     forever #4 s_valid_i <= $urandom_range(0,3);
+  end
+/*------------------------------------------------------------------------------
+--  задавать значения
+------------------------------------------------------------------------------*/
+  initial begin
+    wait(~rst_n);
+    s_data_i[0] <= '0;
+    s_data_i[1] <= '0;
+    s_qos_i[0] <= 1;
+    s_qos_i[1] <= 2;
+    s_last_i <= '0;
+    s_valid_i <= '0;
+    s_last_i <= '0;
+    wait(rst_n);
+    repeat(100) begin
+      @(posedge clk_i);
+      s_valid_i = '1;
+      if (s_ready_o !== 0) begin
+        s_data_i[0] <= $urandom();
+        s_data_i[1] <= $urandom();
+        s_last_i <= $urandom_range(0,2);
+      end
+      if (m_last_o) begin
+        s_qos_i[0] <= $urandom_range(2,15);
+        s_qos_i[1] <= $urandom_range(1,15);
+      end
+    end
+    $stop();
   end
 
-  initial begin 
-    wait_clk(100);
-    forever #4 m_ready_i <= $urandom_range(0,1);
+  initial begin
+    packet p;
+    wait(rst_n);
+    forever begin
+      @(posedge clk_i);
+      if ((s_valid_i[0] && s_ready_o[0]) || (s_valid_i[1] && s_ready_o[1])) begin
+        if ((s_qos_i[0] > s_qos_i[1])  && s_qos_i[0] !== 0 && s_qos_i[1] !== 0 ) begin
+          p.sdata = s_data_i[0];
+          p.qos = s_qos_i[0];
+          in.put(p);
+        end else if (s_qos_i[0] < s_qos_i[1] && s_qos_i[1] !== 0 && s_qos_i[0] !== 0) begin
+          p.sdata = s_data_i[1];
+          p.qos = s_qos_i[1];
+          in.put(p);
+        end
+      end
+    end
+  end
 
+initial begin
+  packet p;
+  wait(rst_n);
+  forever begin
+    @(posedge clk_i);
+    if (m_valid_o && m_ready_i) begin
+      wait_clk(1);
+      p.sdata = m_data_o;
+      p.id = m_id_o;
+      p.qos = m_qos_o;
+      out.put(p);
+    end
+  end
+end
+
+initial begin   // проверка пакетов (работает не корректно)
+  packet in_p, out_p;
+  forever begin 
+    in.get(in_p);
+    out.get(out_p);
+      if (in_p.qos !== out_p.qos && m_valid_o  ) begin 
+        $warning("%0t Invalid priority REAL:%d EXPECTED:%d",
+        $time(), out_p.qos, in_p.qos);
+    end   
+      if (in_p.sdata !== out_p.sdata && m_valid_o) begin  
+       $warning("Invalid DATA");
+      end 
+    end 
   end 
 
-endmodule
+  assign data_1 = s_data_i[0];
+  assign data_2 = s_data_i[1];
+  assign qos_1  = s_qos_i[0];
+  assign qos_2  = s_qos_i[1];
+
+  endmodule
