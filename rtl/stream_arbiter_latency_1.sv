4e2750aebfa597f7fc6d6116cd6d6e36f5bb77cf
fedejone <131912870+fedejone@users.noreply.github.com>
2024-01-15 17:55:42 +0300

    Update stream_arbiter_latency_1.sv

--- rtl/stream_arbiter_latency_1.sv
+++ rtl/stream_arbiter_latency_1.sv
@@ -1,19 +1,5 @@
 /*------------------------------------------------------------------------------
--- арбитр реализован на основе конечного автомата с двумя состояниями :
-1. выбор приоритетного потока 
-2. выдача данных с выбранного потока 
-
-Принцип Round-Robin реализован счетсчиком до 3 пакетов с одного потока при одной
-и той же приоритетности на потоках или при при приоритетности равной 0 на одном из потоков.
-При одной и той же приоритетности потоков, первым на выход выбирается 1 поток.
-
-Модуль буфферизирует данные на один такт, для реализации последовательстной логики и
-синхронизации схемы. (Latency = 1)
-
-При изменении количества потоков, необходимо будет изменить логику выходного сигнала s_ready_o
-(изменить значения 113 и 115 строка), а так же добавить кол-во переборов в 72, 75, 79 строке
-
-
+-- 
 ------------------------------------------------------------------------------*/
 module stream_arbiter_latency_1 #(
   parameter T_DATA_WIDTH = 8                   ,
@@ -42,90 +28,105 @@ module stream_arbiter_latency_1 #(
 /*------------------------------------------------------------------------------
 -- internal signals
 ------------------------------------------------------------------------------*/
-  logic [T_ID___WIDTH-1:0] select_stream_reg; // регистр выбраного потока 
-  logic [             1:0] cnt_rr           ; // счетчик для Round-Robin очереди 
-  logic                    rr_reg           ; // регистр для Round-Robin очереди 
-
-  enum logic [2:0] {IDLE, DATA} state; // состояния автомата
+  logic [T_ID___WIDTH-1:0] select_stream_reg  ; // регистр выбраного потока
+  logic [             1:0] cnt_rr             ; // счетчик для Round-Robin очереди
+  logic [T_ID___WIDTH-1:0] rr_reg             ; // регистр для Round-Robin очереди
+  logic [T_QOS__WIDTH-1:0] select_qos_reg     ; // регистр выбранной приоритетности
+  logic [STREAM_COUNT-1:0] same_priorities_reg; // сигналы с одинаковой приоритетностью
+  logic [  T_ID___WIDTH:0] quantity_one_prior ; // количесвто одинаковых сигнлаов
+  enum logic [1:0] {IDLE, QUEUE, DATA} state;   // состояния автомата
 /*------------------------------------------------------------------------------
---
+-- выбор наибольшей приоритетноти и счет кол-ва одинаковых приоритетностей 
+------------------------------------------------------------------------------*/
+  always_comb begin : proc_select_qos_reg
+    select_qos_reg     = '0;
+    quantity_one_prior = '0;
+    for ( int i = 0; i < STREAM_COUNT ; i++ ) begin // цикл  перебора потоков
+      begin
+        if ((s_qos_i[i] > select_qos_reg) && s_valid_i[i])
+          select_qos_reg = s_qos_i[i]; // присвоение максимальной приоритетности
+      end
+      if (same_priorities_reg[i])
+        quantity_one_prior = quantity_one_prior + 1'b1; // счет кол-ва одинаковых потоков в очереди
+    end
+  end
+/*------------------------------------------------------------------------------
+--  автомат трех состояний 
+IDLE - выбор приориеттного потока и фиксация этого потока в регистр same_priorities_reg
+QUEUE - при одинкаовых или 0 приоритетности циклическая смена потоков каждые два пакета
+DATA - выдача данных выбранного потока и выставление ready_o
 ------------------------------------------------------------------------------*/
   always_ff @( posedge clk_i or negedge rst_n ) begin
     if( ~rst_n ) begin
-      select_stream_reg <= '0;
-      m_data_o          <= '0;
-      m_qos_o           <= '0;
-      m_id_o            <= '0;
-      m_last_o          <= '0;
-      m_valid_o         <= '0;
-      s_ready_o         <= '0;
-      cnt_rr            <= '0;
-      rr_reg            <= '0;
-      state             <= IDLE;
+      select_stream_reg   <= '0;
+      same_priorities_reg <= '0;
+      m_data_o            <= '0;
+      m_qos_o             <= '0;
+      m_id_o              <= '0;
+      m_last_o            <= '0;
+      m_valid_o           <= '0;
+      s_ready_o           <= '0;
+      cnt_rr              <= '0;
+      rr_reg              <= '0;
+      state               <= IDLE;
     end else if ( m_ready_i ) begin // конечный автомат с двумя состояниями определия приоритетного потока и выдачи данных
       case ( state )
         IDLE :
+          begin                        // при выборе приоритетного сигнала модуль не готов к принятию данных
+            s_ready_o           <= '0;
+            m_last_o            <= '0;
+            m_valid_o           <= '0;
+            same_priorities_reg <= '0; // сбросить маску на одинаковые потоки для выставления новой
+            if (s_valid_i) begin       // если вадида нет, то данные на выход не идут
+              for ( int i = 0; i < STREAM_COUNT; i++ ) begin // цикл  перебора потоков для выделения максимального
+                if ( (s_qos_i[i] == select_qos_reg) && s_valid_i[i] || (s_qos_i[i] == 0) && s_valid_i[i])
+                  begin
+                    select_stream_reg      <= i;
+                    same_priorities_reg[i] <= 1'b1; // маска сигналов максимального приоритета
+                  end
+              end  // конец цикла
+              state <= QUEUE;
+            end
+          end
+        QUEUE :
           begin
-            s_ready_o <= '0;        // при выборе приоритетного сигнала модуль не готов к принятию данных
+            s_ready_o <= '0;
             m_last_o  <= '0;
-            for ( int i = 1; i < STREAM_COUNT; i++ ) begin              // цикл  перебора двух потоков
-              // сравнение двух потоков на более приотритеный без учета 0 приоритетности b одинаковых приоритетов
-              if ( (s_qos_i[i] !== s_qos_i[i-1]) && (s_qos_i[i] !== 0 && s_qos_i[i-1] !== 0))
-                begin
-                  cnt_rr <= '0;                                         // сброс очереди Round-Robin
-                  if ( (s_qos_i[i] > s_qos_i[i-1]) && s_valid_i[i]  )   // приоритет у второго потока
-                    begin
-                      select_stream_reg <= i;
-                    end
-                  else if (s_qos_i[i] < s_qos_i[i-1] && s_valid_i[i-1]) // приоритет у первого потока
-                    begin
-                      select_stream_reg <= i-1;
-                    end
-                  else if (s_valid_i[i])   // приоритет уходит единственному валидному потоку
-                    begin
-                      select_stream_reg <= i;
-                    end
-                end
-              else if ( &s_valid_i ) begin // очередь Round-Robin если одинаковые приоритетности и они валидны
-                cnt_rr <= cnt_rr + 1'b1;   // Round-Robin до трех итераций одного потока
-                if ( cnt_rr == STREAM_COUNT )
-                  begin
-                    cnt_rr <= '0;
-                  end
-                if (cnt_rr == STREAM_COUNT)
-                  begin
-                    rr_reg <= ~rr_reg;
-                  end
-                select_stream_reg <= rr_reg; //{select_stream_reg[T_ID___WIDTH-1:1], rr_reg}; // при одной приоритетности первым пойдет первый поток
+            if ( quantity_one_prior > 1 ) begin // очередь Round-Robin если одинаковые приоритетности
+              cnt_rr <= cnt_rr + 1'b1;   //  счет кол-ва повторений прихода одной проиритетности
+              for ( int i = 0; i < STREAM_COUNT; i++ ) begin // цикл колцевой смены потоков
+                if ( same_priorities_reg[i] && rr_reg == i ) // по маске same_priorities_reg и регистру rr_reg выбор следующего потока с одним приоритетом
+                  select_stream_reg <= i;
+              end // конец цикла
+              if ( cnt_rr == 2 ) begin // смена потока одинаковых приоритетностей каждые два такта
+                cnt_rr <= '0;
+                if ( rr_reg == quantity_one_prior-1 )  // сброс, при достижении кол-ва одинаковых потоков
+                  rr_reg <= '0;
+                else
+                  rr_reg <= rr_reg + 1'b1; // кольцевая смена потоков, начиная с первого
               end
             end
-            if ( s_valid_i )  begin          // если вадида нет, то данные на выход не идут
-              state     <= DATA;
-              m_valid_o <= '0;
-            end else
-            begin
-              m_valid_o <= '0;
-              s_ready_o <= '1;
+            else begin
+              cnt_rr <= '0; // сброс регистров round-robin при отсутсвии одинаковых приоритетов
+              rr_reg <= '0;
             end
+            state     <= DATA;
+            m_valid_o <= '0;
           end
         DATA :
           begin
-            if (s_valid_i[select_stream_reg]) begin // на выход идут только валидные данные 
-              m_data_o  <= s_data_i[select_stream_reg]; // данные выбраного потока
-              m_id_o    <= select_stream_reg;           // номер выбранного потока
-              m_last_o  <= s_last_i[select_stream_reg]; // данные корректны
-              m_valid_o <= '1;
-              m_qos_o   <= s_qos_i[select_stream_reg];
-              if (select_stream_reg) begin
-                s_ready_o <= {select_stream_reg , 1'b0}; // выходная готовность используемого потока для slave модуля
-              end else begin
-                s_ready_o <= {1'b0 , ~select_stream_reg};
-              end
+            if (s_valid_i[select_stream_reg]) begin // на выход идут только валидные данные
+              m_data_o                     <= s_data_i[select_stream_reg]; // данные выбраного потока
+              m_id_o                       <= select_stream_reg;           // номер выбранного потока
+              m_last_o                     <= s_last_i[select_stream_reg]; // данные корректны
+              m_valid_o                    <= '1;
+              m_qos_o                      <= s_qos_i[select_stream_reg];
+              s_ready_o[select_stream_reg] <= 1'b1;                        // сигнла ready отправляется только выбранному потоку
               if (s_last_i[select_stream_reg]) begin
-                state <= IDLE;                           // при достижении конца пакета переход на выбор потока
+                state <= IDLE;  // при достижении конца пакета переход на выбор потока
               end
             end
-            else begin // при отсутсвии валида на выбранном потоке ожидание 
+            else begin // при отсутсвии валида на выбранном потоке - ожидание
               m_data_o  <= m_data_o;
               m_id_o    <= m_id_o;
               m_last_o  <= m_last_o;
@@ -134,21 +135,18 @@ module stream_arbiter_latency_1 #(
               s_ready_o <= s_ready_o;
             end
           end
-        default :
-          begin
-            m_data_o  <= m_data_o;
-            m_id_o    <= m_id_o;
-            m_last_o  <= '0;
-            m_valid_o <= '0;
-            m_qos_o   <= m_qos_o;
-            s_ready_o <= '0;
-            state     <= IDLE;
-          end
       endcase
-    end else if ( ~m_ready_i ) begin
+    end else begin // ожиданеие при отсутсвии ready_i
       m_valid_o <= '0;
       s_ready_o <= '0;
+      m_data_o  <= m_data_o;
+      m_id_o    <= m_id_o;
+      m_last_o  <= m_last_o;
+      m_qos_o   <= m_qos_o;
     end
   end
+
+  
+
 
 endmodule 
